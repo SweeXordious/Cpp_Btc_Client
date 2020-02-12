d551ddd9251d629cfad3c4b78ba3be6c9fb0ca96
CHAMI Rachid <chamirachid1@gmail.com>
2020-02-12 09:29:58 -0800

    Fixed reqs file

--- reqs.sh
+++ reqs.sh
@@ -1,10 +1,10 @@
 sudo apt-get update
 
 # Install libgmp for multiprecision calculations
-sudo apt-get install libgmp3-dev
+sudo apt-get install -y libgmp3-dev
 
 # Install libsecp256k1 for ecdsa operations
-sudo apt-get install git autoconf
+sudo apt-get install -y git autoconf
 git clone https://github.com/bitcoin-core/secp256k1
 ./autogen.sh
 ./configure
