b6066db545934a1103db724be426247922ba4834
CHAMI Rachid <chamirachid1@gmail.com>
2020-02-12 09:33:13 -0800

    Fixed reqs file

--- reqs.sh
+++ reqs.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y libgmp3-dev
 # Install libsecp256k1 for ecdsa operations
 sudo apt-get install -y git autoconf
 git clone https://github.com/bitcoin-core/secp256k1
+cd secp256k1
 ./autogen.sh
 ./configure
 make
